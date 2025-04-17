6aba89be323afeaff7b7b50708a47de94122a594
meganathan <meganathan212003@gmail.com>
2025-04-17 22:27:02 +0530

    write the comments

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+/**
+ * This is the axios file for write the 
+ * base url and use where we need this
+ * 
+ * @author meganathan
+ */
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.VITE_API_LOCAL_URL,
     withCredentials: true,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['37b5-2409-40f4-411a-a86f-3163-c579-7919-3f62.ngrok-free.app']
+  }
 })
